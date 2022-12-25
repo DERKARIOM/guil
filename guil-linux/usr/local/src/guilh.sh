c700ea7a91de0f6da3280ddd8b2dcfbbeceb7a40
DERKARIOM5456 <derkariom54565654@gmail.com>
2022-12-25 17:18:57 +0100

    debut du projet

---
+++ guil-linux/usr/local/src/guilh.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+cherche()
+{
+    for arg in "$@"
+    do
+        if [ "$arg" != "-o" -a "$arg" != "-x" -a "$arg" != "-ox" ] ; then
+            tab[$i]="$arg"
+            i+=1
+        else
+            break;
+        fi
+    done
+    return $i
+}
+version()
+{
+    echo "Version Guil 1.0 beta :)"
+}
+help()
+{
+    echo "En cours de developement"
+}
