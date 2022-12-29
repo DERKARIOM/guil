6aa6a23c2fa65f11004b056ab250e9e5da577b92
DERKARIOM5456 <derkariom54565654@gmail.com>
2022-12-29 07:52:54 +0100

    fix (*) : Correction des bug

--- guil-linux/usr/local/src/guilh.sh
+++ guil-linux/usr/local/src/guilh.sh
@@ -14,9 +14,44 @@ cherche()
 }
 version()
 {
-    echo "Version Guil 1.1 beta :)"
+    echo "Graphic User Interface Librery (GUIL) 1.5"
 }
 help()
 {
-    echo "En cours de developement"
+    echo -e "guil -v : Pour afficher la version de guil\n"
+    echo -e "guil <fichier> -o <executable> : Pour compiler le fichier\n"
+    echo -e "guil <fichier> -ox <executable> : Pour compiler puis executer\n"
+    echo -e "xguil ? : Pour afficher les langage ou gui appliquez\n"
+    echo -e "xguil c : Pour appliquez le langage c\n"
+    echo -e "xguil c++ : Pour appliquez le langage c++\n"
+    echo -e "xguil gtk : Pour appliquez gtk\n"
+    echo -e "xguil gtkmm : Pour appliquez gtkmm\n"
+    echo -e "xguil autocode.true <repertoire> : Pour appliquez l' auto completion de code\n"
+    echo -e "xguil autocode.false <repertoire> : Pour supprimer l' auto completion du code\n"
+}
+xhelp()
+{
+    echo -e "xguil ? : Pour afficher les langage ou gui appliquez\n"
+    echo -e "xguil c : Pour appliquez le langage c\n"
+    echo -e "xguil c++ : Pour appliquez le langage c++\n"
+    echo -e "xguil gtk : Pour appliquez gtk\n"
+    echo -e "xguil gtkmm : Pour appliquez gtkmm\n"
+    echo -e "xguil autocode.true <repertoire> : Pour appliquez l' auto completion du code\n"
+    echo -e "xguil autocode.false <repertoire> : Pour supprimer l' auto completion du code\n"
+}
+info()
+{
+    echo "**************************************************************************"
+    echo "# NOM: GRAPHIC USER INTERFACE LIBRERY (GUIL)                             #"
+    echo "# VERSION: 1.5                                                           #"
+    echo "# INTERPRETEUR DE: C,C++,GTK ET GTKMM                                    #"
+    echo "# LICENCE: LIBRE                                                         #"
+    echo "# CONSERNE: TOUTE PERSONNE AYANT DES CONNAISSANCES SUR LA PROGRAMATION   #"
+    echo "# DATE:01/01/2023                                                        #"
+    echo "# NATIONALITE: NIGERIENNE                                                #"
+    echo "# AUTEUR: BACHIR ABDOUL KADER                                            #" 
+    echo "# PROFESION: ETUDIANT A L' UNIVERSITE ABDOU MOUMOUNI DE NIAMEY           #" 
+    echo "# CLUB: LES VISIONNAIRES                                                 #"
+    echo "# PROJET: DERKARIOM                                                      #"
+    echo "**************************************************************************"                     
 }
